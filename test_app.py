a96597a305221638cb78da6354b11b5e91e64c57
keTan199 <singhketan1996@gmail.com>
2024-07-15 21:14:59 +0530

    CI CD Check V2

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ def client():
 
 def test_home(client):
     response = client.get('/ping')
-    assert response.status_code == 404
+    assert response.status_code == 200
 
 def test_root(client):
     response = client.get('/')
